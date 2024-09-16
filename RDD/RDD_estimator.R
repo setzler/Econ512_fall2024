76047609e0897156f8c6492c081f1ea86308eb41
Bradley Setzler <bradley.setzler@gmail.com>
2024-09-16 18:12:57 -0400

    We successfully tested the simple RDD estimator.

--- RDD/RDD_estimator.R
+++ RDD/RDD_estimator.R
@@ -23,4 +23,21 @@ RDD_estimator_simple <- function( inputdata, c_threshold_point = 0, tau_bandwidt
     return( RDD_beta_hat )
 }
 
+# loop across threshold choices and see which threshold gives the best estimate
+thresholds = seq(0.1, 1.0, by = 0.1)
 
+results = rep(0, length(thresholds) )
+for( i in 1:length(thresholds) ){
+    results[i] = RDD_estimator_simple( the_data_set, c_threshold_point = 0, tau_bandwidth = thresholds[i] )
+}
+
+# plot the results, with a horizontal line at 3.0 
+gg = ggplot(data = data.table( thresholds, results ), aes(x=thresholds, y=results)) + 
+    geom_point() + 
+    geom_line() + 
+    theme_bw(base_size = 16) + 
+    labs(x="Bandwidth (tau)", y="Treatment Effect Estimate") +
+    geom_hline(yintercept = 3.0, linetype="dashed", color = "red") +
+    ylim(0, 6)
+
+ggsave(gg, file="RDD_estimator_simple.pdf", width=8, height=5)
